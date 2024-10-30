785351f83bad23f918af81f251765b7d1569b780
amiro hatami <amirohatami@amiros-Laptop.local>
2024-10-30 10:21:31 +0330

    get data with async and await, and add another stet for the data fetching  and showing some massage for slow internet

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [ isFetching, setIsFetching] = useState(false)
   const [availablePlaces, setAvailablePlaces] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      setIsFetching(true)
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+      setIsFetching(false)
+    }
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching Place Data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
